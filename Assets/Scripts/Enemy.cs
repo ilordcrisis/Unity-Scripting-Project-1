af29cd1adb6d37f0ce9d99a4a344e72af7481a28
Fredrick Clay <ilordcrisis@gmail.com>
2025-11-09 17:58:31 -0500

    Pattern Movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,24 +4,35 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
     public GameObject explosionPrefab;
 
     private GameManager gameManager;
 
+    // Speed and movement direction
+    public float speed = 8f;
+    private Vector3 moveDirection;
+
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        // Assign a random diagonal direction (mostly downward)
+        float randomX = Random.Range(-0.5f, 0.5f); // small horizontal variation
+        moveDirection = new Vector3(randomX, -1f, 0f).normalized; // normalize for consistent speed
     }
-    // Update is called once per frame
+
     void Update()
     {
-        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 8f);
-        if (transform.position.y < -6.5f)
+        // Move in the assigned direction
+        transform.Translate(moveDirection * speed * Time.deltaTime);
+
+        // Destroy if off-screen
+        if (transform.position.y < -6.5f || Mathf.Abs(transform.position.x) > 10f)
         {
             Destroy(this.gameObject);
         }
     }
+
 
     private void OnTriggerEnter2D(Collider2D whatDidIHit)
     {
