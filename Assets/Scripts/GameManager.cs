fe5e119734a4c3ba9bb0664ecca977717b78afc4
Greg00811 <gregory081105@gmail.com>
2025-11-09 16:48:26 -0500

    I added 2 more enemy types as well as added a clamp to prevent the player from moving off the bottom of the screen.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject enemyOnePrefab;
+
+    public GameObject enemyTwoPrefab;
+
+    public GameObject enemyThreePrefab;
+
+    public TextMeshProUGUI livesText;
+
+    public int score;
+
+    public float horizontalScreenSize = 6f;
+
+    public float verticalScreenSize = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        InvokeRepeating("CreateEnemyOne", 1, 2);
+        InvokeRepeating("CreateEnemyTwo", 3, 3);
+        InvokeRepeating("CreateEnemyThree", 5, 4);
+    }
+
+    void CreateEnemyOne()
+    {
+        Debug.Log("I am enemy one");
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyTwo()
+    {
+        Debug.Log("I am enemy two");
+        Instantiate(enemyTwoPrefab, new Vector3(Random.Range(-7f, 7f), 6.5f, 0), Quaternion.identity);
+    }
+
+    void CreateEnemyThree()
+    {
+        Debug.Log("I am enemy three");
+        Instantiate(enemyThreePrefab, new Vector3(Random.Range(-3f, 3f), 6.5f, 0), Quaternion.identity);
+    }
+
+    public void AddScore(int earnedScore)
+    {
+        score += earnedScore;
+    }
+
+    public void ChangeLivesText(int currentLives)
+    {
+        livesText.text = "Lives: " + currentLives;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class PlayerController : MonoBehaviour
+{
+    public int lives;
+
+    private float playerSpeed;
+
+    private float horizontalInput;
+
+    private float verticalInput;
+
+    private float halfY;
+
+    private float bottomY;
+
+    private float horizontalScreenLimit = 9.5f;
+
+    private float verticalScreenLimit = 6.5f;
+
+    public GameObject explosionPrefab;
+
+    public GameObject bulletPrefab;
+
+    private GameManager gameManager;
+
+
+    void Start()
+    {
+        playerSpeed = 6f;
+        lives = 3;
+
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager.ChangeLivesText(lives);
+    }
+
+    public void LoseALife()
+    {
+        Debug.Log("Player hit! Lose a life.");
+        lives--;
+        gameManager.ChangeLivesText(lives);
+        if (lives == 0)
+        {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        Shooting();
+
+    }
+
+    void Movement()
+    {
+        //get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //move the player
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * Time.deltaTime * playerSpeed);
+
+        //player can leave the screen horizontally
+        if(transform.position.x > horizontalScreenLimit || transform.position.x < -horizontalScreenLimit)
+        {
+            transform.position = new Vector3(transform.position.x * -1, transform.position.y, 0);
+        }
+
+        //finds the halfway point of the screen
+        float halfY = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height / 2f, Camera.main.transform.position.z * -1)).y;
+        //finds the bottom point of the screen
+        float bottomY = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.transform.position.z * -1)).y;
+
+        // clamp position between bottom and halfway point
+        Vector3 pos = transform.position;
+
+        if(pos.y > halfY)
+        {
+            pos.y = halfY;
+        }
+        
+        if(pos.y < bottomY)
+        {
+            pos.y = bottomY;
+        }
+        
+        transform.position = pos;
+    }
+    
+    void Shooting()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Pew Pew" + verticalScreenLimit);
+            //spawn bullet at player position
+            Instantiate(bulletPrefab, transform.position + new Vector3(0, 1, 0), Quaternion.identity);
+        }
+    }
+}
