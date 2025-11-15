7ca0c3364375d14e934733b9ef387baff31e1684
Greg00811 <gregory081105@gmail.com>
2025-11-15 16:53:11 -0500

    I updated all the scripts as well as making the game still work with our previous added code.

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -1,27 +1,33 @@
+
 using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
+
     private float speed;
 
     private GameManager gameManager;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Start is called before the first frame update
     void Start()
     {
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>(); //look at hierarchy get the game manager object and get its script
-
-        transform.localScale = transform.localScale * Random.Range(0.1f, 0.6f);
-        transform.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, Random.Range(0.1f, 0.7f)); //the last one alpha is transparency
-        speed = Random.Range(3f, 7f);
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        transform.localScale = transform.localScale * Random.Range(0.5f, 1.5f);
+        transform.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, Random.Range(0.1f, 0.7f));
+        speed = Random.Range(1f, 3f) * gameManager.cloudMove;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.down * speed * Time.deltaTime); //move down at speed
-        if(transform.position.y < -gameManager.verticalScreenSize)
+        speed = speed * gameManager.cloudMove;
+
+        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * speed);
+
+        if (transform.position.y < -gameManager.verticalScreenSize)
         {
-            transform.position = new Vector3(Random.Range(-gameManager.horizontalScreenSize, gameManager.horizontalScreenSize), gameManager.verticalScreenSize * 1.2f, 0);
+            transform.position = new Vector3(Random.Range(-gameManager.horizontalScreenSize, gameManager.horizontalScreenSize), gameManager.verticalScreenSize, 0);
         }
+        
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,58 +3,156 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
+using Unity.VisualScripting;
 
 public class GameManager : MonoBehaviour
 {
 
+    public GameObject playerPrefab;
     public GameObject enemyOnePrefab;
-
     public GameObject enemyTwoPrefab;
-
     public GameObject enemyThreePrefab;
+    public GameObject cloudPrefab;
+    public GameObject gameOverText;
+    public GameObject restartText;
+    public GameObject powerupPrefab;
+    public GameObject audioPlayer;
+
+    public AudioClip powerupSound;
+    public AudioClip powerdownSound;
 
     public TextMeshProUGUI livesText;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI powerupText;
+    
+    public float horizontalScreenSize;
+    public float verticalScreenSize;
 
     public int score;
+    public int cloudMove;
 
-    public float horizontalScreenSize = 6f;
-
-    public float verticalScreenSize = 5f;
+    private bool gameOver;
 
     // Start is called before the first frame update
     void Start()
     {
+        horizontalScreenSize = 10f;
+        verticalScreenSize = 7f;
         score = 0;
-        InvokeRepeating("CreateEnemyOne", 1, 2);
+        cloudMove = 1;
+        gameOver = false;
+        AddScore(0);
+        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        CreateSky();
+        InvokeRepeating("CreateEnemy", 1, 3);
         InvokeRepeating("CreateEnemyTwo", 3, 4);
         InvokeRepeating("CreateEnemyThree", 5, 6);
+        StartCoroutine(SpawnPowerup());
+        //powerupText.text = "No powerups yet!";
     }
 
-    void CreateEnemyOne()
+    // Update is called once per frame
+    void Update()
     {
-        Debug.Log("I am enemy one");
-        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-9f, 9f), 6.5f, 0), Quaternion.identity);
+        if(gameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    void CreateEnemy()
+    {
+        Instantiate(enemyOnePrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.8f, verticalScreenSize, 0), Quaternion.identity);
     }
 
     void CreateEnemyTwo()
     {
         Debug.Log("I am enemy two");
-        Instantiate(enemyTwoPrefab, new Vector3(Random.Range(-7f, 7f), 6.5f, 0), Quaternion.identity);
+        Instantiate(enemyTwoPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.8f, verticalScreenSize, 0), Quaternion.identity);
     }
 
     void CreateEnemyThree()
     {
         Debug.Log("I am enemy three");
-        Instantiate(enemyThreePrefab, new Vector3(Random.Range(-3f, 3f), 6.5f, 0), Quaternion.identity);
+        Instantiate(enemyThreePrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize) * 0.8f, verticalScreenSize, 0), Quaternion.identity);
+    }
+
+    void CreatePowerup()
+    {
+        Instantiate(powerupPrefab, new Vector3(Random.Range(-horizontalScreenSize * 0.8f, horizontalScreenSize * 0.8f), Random.Range(-verticalScreenSize * 0.8f, verticalScreenSize * 0.8f), 0), Quaternion.identity);
+    }
+
+    void CreateSky()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloudPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize), Random.Range(-verticalScreenSize, verticalScreenSize), 0), Quaternion.identity);
+        }
+        
+    }
+
+    public void ManagePowerupText(int powerupType)
+    {
+        switch (powerupType)
+        {
+            case 1:
+                powerupText.text = "Speed!";
+                break;
+            case 2:
+                powerupText.text = "Double Weapon!";
+                break;
+            case 3:
+                powerupText.text = "Triple Weapon!";
+                break;
+            case 4:
+                powerupText.text = "Shield!";
+                break;
+            default:
+                powerupText.text = "No powerups yet!";
+                break;
+        }
+    }
+
+    IEnumerator SpawnPowerup()
+    {
+        float spawnTime = Random.Range(3, 5); 
+        yield return new WaitForSeconds(spawnTime);
+        CreatePowerup();
+        StartCoroutine(SpawnPowerup());
+    }
+
+    public void PlaySound(int whichSound)
+    {
+        switch (whichSound)
+        {
+            case 1:
+                audioPlayer.GetComponent<AudioSource>().PlayOneShot(powerupSound);
+                break;
+            case 2:
+                audioPlayer.GetComponent<AudioSource>().PlayOneShot(powerdownSound);
+                break;
+        }
     }
 
     public void AddScore(int earnedScore)
     {
-        score += earnedScore;
+        score = score + earnedScore;
+       //scoreText.text = "Score: " + score;
     }
 
-    public void ChangeLivesText(int currentLives)
+    public void ChangeLivesText (int currentLives)
     {
-        livesText.text = "Lives: " + currentLives;
+        //livesText.text = "Lives: " + currentLives;
     }
+
+    public void GameOver()
+    {
+        gameOverText.SetActive(true);
+        restartText.SetActive(true);
+        gameOver = true;
+        CancelInvoke();
+        cloudMove = 0;
+    }
+
 }
